52c980455d118a4ae9aab52653772387d6fdc492
cplummer-mn <c.plummer@everafterdigital.com>
2022-12-20 02:20:33 -0600

    Added logic to calculator. Some UI
    Added all logic for calculator and fixed a few bugs in some
    edge cases such as pressing operators multiple time in a row.
    Also added divide by zero logic.

---
+++ javascript.js
@@ -0,0 +1,140 @@
+function add(x,y) {
+    return (x + y);
+}
+
+function subtract(x,y) {
+    return (x - y);
+}
+
+function multiply(x,y) {
+    return (x * y);
+}
+
+function divide(x,y) {
+    return (x / y);    
+}
+
+function operate(o,x,y) {
+    let numOne = +x;
+    let numTwo = +y;
+    if(o == '+') {
+        return add(numOne,numTwo);
+    }
+    else if(o == '-') {
+        return subtract(numOne,numTwo);
+    }
+    else if(o == '*') {
+        return multiply(numOne,numTwo);
+    }
+    else if(o == '/') {
+        return divide(numOne,numTwo);
+    }
+}
+
+var firstNum = '';
+var secondNum = '';
+var operator = '';
+
+let result = 0;
+let waiting = true;
+let justSolved = false;
+let justOperated = false;
+
+const displayValue = document.querySelector('#display');
+const displayValueTwo = document.querySelector('#displayTwo');
+
+displayValueTwo.textContent = '';
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.id == '/' || button.id == '*' || button.id == '-' || button.id == '+' ){
+            if (justOperated) {
+                return;
+            }
+            else if (justSolved) {
+                displayValueTwo.textContent = firstNum + ' ' + operator + ' ' + secondNum
+                    + ' = ' + result + ' ' + `${button.id}`;
+                    operator = button.id;
+                    firstNum = result;
+                    waiting = true;
+                    justSolved = false;
+                    justOperated = true;
+            }
+            else if(!firstNum == ''){
+                secondNum = displayValue.textContent;
+                result = operate(operator,firstNum,secondNum);
+                displayValueTwo.textContent = firstNum + ' ' + operator + ' ' + secondNum
+                    + ' = ' + result; 
+                firstNum = result;
+                operator = button.id;
+                waiting = true;
+                justOperated = true;
+            }
+            else {
+            firstNum = displayValue.textContent;
+            operator = button.id;
+            displayValueTwo.textContent = firstNum + " " + operator;
+            waiting  = true;
+            justOperated = true;
+            }
+            
+        }
+        else if(button.id == '=') {
+            if(operator == '/' && displayValue.textContent == 0) {
+                alert('NICE TRY BUDDY. THAT DOESN\'T WORK');
+                clearCalc();
+            }
+            else {
+            secondNum = displayValue.textContent;
+            result = operate(operator,firstNum,secondNum);
+            displayValueTwo.textContent = firstNum + ' ' + operator + ' ' + secondNum
+            + ' =';
+            displayValue.textContent = result;
+            justSolved = true;
+            justOperated = false;
+            }
+        }
+        else if(button.id == 'clear') {
+            clearCalc();
+        }
+        else if (button.id == 'delete') {
+            displayValue.textContent = displayValue.textContent.slice(0,-1);
+            if(displayValue.textContent.length == 0) {
+                displayValue.textContent = 0;
+            }
+        }
+        else if(displayValue.textContent == '0' || waiting) {
+            displayValue.textContent = button.id;
+            waiting = false;
+            justOperated = false;
+        }
+        else {
+            if(displayValue.textContent.length > 9) {
+                return;
+            }
+            else { 
+                displayValue.textContent += button.id;
+                justOperated = false;
+            }
+        }
+         //DEBUG CODE
+        console.log('firstNum = ' + firstNum, 
+        'secondNum = ' + secondNum,
+        'operator = ' + operator,
+        'result = ' + result,
+        'waiting = ' + waiting,
+        'justSolved = ' + justSolved);
+    });
+});
+
+function clearCalc() {
+    displayValue.textContent = '0';
+    displayValueTwo.textContent = '';
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    result = 0;
+    justSolved = false;
+    justOperated = false;
+}
